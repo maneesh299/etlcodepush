a9b0fb0fbe8efba497c1585429cff3f589dc4ae4
Maneesh Murali <maneesh299@gmail.com>
2023-06-20 16:30:21 +0000

    test file contents

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
     db.task.insert_many(csv_data)
     
     return jsonify(
-        message="Welcome to Dyson task app version 5! I am running inside pod!"
+        message="Welcome to Dyson tasks app version 5! I am running inside pod!"
     )
 
 
