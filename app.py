8b0097051cd7ac59b5481d79b9fc780c0030df68
Maneesh Murali <maneesh299@gmail.com>
2023-06-21 09:29:39 +0000

    final changes

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from google.cloud import storage
 import pandas as pd
 import json
 import socket
+import configparser
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://mongo:27017/dev"
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+mongo_uri = config.get('Config', 'DBURI')
+
+app.config['MONGO_URI'] = mongo_uri
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 mongo = PyMongo(app)
 db = mongo.db
@@ -15,14 +21,14 @@ db = mongo.db
 
 @app.route("/")
 def index():
-    bucket_name = "eng-district-390211"
-    file_name = "input.csv"
+    bucket_name = config.get('Config', 'GCP_PROJECT')
+    file_name = config.get('Config', 'FILE_NAME')
     csv_data = pd.DataFrame(pd.read_csv('gs://' + bucket_name + '/' + file_name, encoding='utf-8', encoding_errors='ignore'))
     csv_data = csv_data.to_dict(orient="records")
     db.task.insert_many(csv_data)
     
     return jsonify(
-        message="Welcome to Dyson tasks should work for  app version 5! I am running inside pod!"
+        message="Welcome to file read from config! I am converting csv to document structure in json!"
     )
 
 
@@ -41,46 +47,7 @@ def get_all_tasks():
     )
 
 
-@app.route("/task", methods=["POST"])
-def create_task():
-    data = request.get_json(force=True)
-    db.task.insert_one({"task": data["task"]})
-    return jsonify(
-        message="Task saved successfully!"
-    )
 
-
-@app.route("/task/<id>", methods=["PUT"])
-def update_task(id):
-    data = request.get_json(force=True)["task"]
-    response = db.task.update_one({"_id": ObjectId(id)}, {"$set": {"task": data}})
-    if response.matched_count:
-        message = "Task updated successfully!"
-    else:
-        message = "No Task found!"
-    return jsonify(
-        message=message
-    )
-
-
-@app.route("/task/<id>", methods=["DELETE"])
-def delete_task(id):
-    response = db.task.delete_one({"_id": ObjectId(id)})
-    if response.deleted_count:
-        message = "Task deleted successfully!"
-    else:
-        message = "No Task found!"
-    return jsonify(
-        message=message
-    )
-
-
-@app.route("/tasks/delete", methods=["POST"])
-def delete_all_tasks():
-    db.task.remove()
-    return jsonify(
-        message="All Tasks deleted!"
-    )
 
 
 if __name__ == "__main__":
